5b64a885346b0ef289701b1087894e9a5874db9f
Hilal Rana Anarsay <ranarsay@gmail.com>
2022-06-04 14:58:55 +0300

    update on random images

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
+
 for (i = 1; i < 16; i++) {
-    document.getElementById(`randomImg${i}`).src = "https://source.unsplash.com/random/200x200";
+    document.getElementById(`randomImg${i}`).src = "https://source.unsplash.com/random/200x200/?sig="+ Math.floor(Math.random()*100);
 }
 
